f0bec7069daa3e1069448a87d9e2b8806a9d736d
AbhinasDash <abhinasdash143@gmail.com>
2023-04-24 09:12:23 +0530

    Verify e-mail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import AuthContext from "../store/AuthContext";
 
 const Home = () => {
+  const ctx = useContext(AuthContext);
+
+  const emailHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyC5RfB2zeAPwPIykibRKYnL7KdPnkq49Bw",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: ctx.token,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          res.json().then((data) => {
+            let errorMsg = "Authotication Failed";
+            if (data && data.error && data.error.message) {
+              errorMsg = data.error.message;
+            }
+            throw new Error(errorMsg);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <>
       <header className="header">
@@ -10,7 +48,14 @@ const Home = () => {
         <h3 className="profile">
           Your profile is incomplete <Link to="Profile">Complete now</Link>
         </h3>
+        <br />
       </header>
+      <div className="verify">
+        <h2>Verify your email </h2>
+        <button className="bt" onClick={emailHandler}>
+          Verify E-mail
+        </button>
+      </div>
     </>
   );
 };
